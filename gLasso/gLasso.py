77343f8934c44de5c0b5faefb6cd89ac4dbb4f1f
Jeremiah Zhe Liu <zhel90@gmail.com>
2018-11-24 21:47:23 -0500

    fixed shaped alignment, added model weight extraction

--- gLasso/gLasso.py
+++ gLasso/gLasso.py
@@ -1,4 +1,3 @@
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -12,6 +11,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--batch_size', default=100, type=int, help='batch size')
 parser.add_argument('--train_steps', default=1000, type=int,
                     help='number of training steps')
+
 
 def gLasso_model(features, labels, mode, params):
     """DNN with three hidden layers, and dropout of 0.1 probability."""
@@ -33,6 +33,7 @@ def gLasso_model(features, labels, mode, params):
 
     # Compute logits (1 per class).
     response = tf.layers.dense(net, params['n_response'], activation=None)
+    response = tf.squeeze(response)
 
     # Compute predictions.
     if mode == tf.estimator.ModeKeys.PREDICT:
@@ -42,14 +43,14 @@ def gLasso_model(features, labels, mode, params):
         return tf.estimator.EstimatorSpec(mode, predictions=predictions)
 
     # Compute loss.
-    loss = tf.losses.mean_squared_error(labels=tf.case(tf.expand_dims(labels, axis=1), tf.float32),
+    loss = tf.losses.mean_squared_error(labels=labels,
                                         predictions=response)
 
     # Compute evaluation metrics.
-    mse = tf.metrics.mean_squared_error(labels=tf.case(tf.expand_dims(labels, axis=1), tf.float32),
+    mse = tf.metrics.mean_squared_error(labels=labels,
                                         predictions=response)
     metrics = {'MSE': mse}
-    tf.summary.scalar("MSE", mse)
+    tf.summary.scalar("MSE", mse[1])
 
     if mode == tf.estimator.ModeKeys.EVAL:
         return tf.estimator.EstimatorSpec(
@@ -60,7 +61,7 @@ def gLasso_model(features, labels, mode, params):
 
     optimizer = tf.train.AdagradOptimizer(learning_rate=0.1)
     train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
-    layer1_weight = tf.trainable_variables("layer1")
+
     return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
 
 
@@ -73,7 +74,8 @@ def main(argv):
     # Feature columns describe how to use the input.
     my_feature_columns = []
     for key in train_x.keys():
-        my_feature_columns.append(tf.feature_column.numeric_column(key=key))
+        my_feature_columns.append(
+            tf.feature_column.numeric_column(key=key))
 
     # Build 2 hidden layer DNN with 10, 10 units respectively.
     classifier = tf.estimator.Estimator(
@@ -88,18 +90,22 @@ def main(argv):
 
     # Train the Model.
     classifier.train(
-        input_fn=lambda:simu_data.train_input_fn(train_x, train_y, args.batch_size),
+        input_fn=lambda: simu_data.train_input_fn(
+            train_x, train_y, args.batch_size),
         steps=args.train_steps)
 
     # Evaluate the model.
     eval_result = classifier.evaluate(
-        input_fn=lambda:simu_data.eval_input_fn(test_x, test_y, args.batch_size))
+        input_fn=lambda: simu_data.eval_input_fn(test_x, test_y, args.batch_size))
 
-    print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
+    # extract variables from model
+    var_dict = dict()
+    for var_name in classifier.get_variable_names():
+        var_dict[var_name] = classifier.get_variable_value(var_name)
 
+    print('\nTest set MSE: {MSE:0.3f}\n'.format(**eval_result))
 
 
 if __name__ == '__main__':
     tf.logging.set_verbosity(tf.logging.INFO)
     tf.app.run(main)
-
